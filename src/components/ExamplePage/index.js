09d3953ce5510bf708f090d1b27ea0a454e073b5
Simon Douchy <simon.douchy@gmail.com>
2019-12-26 10:02:33 +0100

    Example page with lazy loading

--- src/components/ExamplePage/index.js
+++ src/components/ExamplePage/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import styled from 'styled-components';
+import { Switch, Route } from 'react-router-dom';
 
 import colors from 'styles/colors';
 
 import HeaderBar from 'components/HeaderBar';
-import { Switch, Route } from 'react-router-dom';
+import ExamplePane from 'components/ExamplePane';
+
+const CounterHooks = lazy(() => import('components/CounterHooks'));
+const CounterRedux = lazy(() => import('components/CounterRedux'));
 
 const PageContainer = styled.div`
   display: flex;
@@ -18,8 +22,20 @@ const ExamplePage = () => {
     <PageContainer>
       <HeaderBar />
       <Switch>
-        <Route path="/hooks">hooks</Route>
-        <Route path="/redux">redux</Route>
+        <Route path="/hooks">
+          <ExamplePane title="hooks" description="hooks Example">
+            <Suspense fallback={<div>Loading ...</div>}>
+              <CounterHooks />
+            </Suspense>
+          </ExamplePane>
+        </Route>
+        <Route path="/redux">
+          <ExamplePane title="redux" description="redux Example">
+            <Suspense fallback={<div>Loading ...</div>}>
+              <CounterRedux />
+            </Suspense>
+          </ExamplePane>
+        </Route>
         <Route path="/others">others</Route>
       </Switch>
     </PageContainer>
