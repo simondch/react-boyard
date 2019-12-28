867e327e32a69bd261e093aedf3d13513d47cf7d
Simon Douchy <simon.douchy@gmail.com>
2019-12-28 18:22:57 +0100

    initial build commit

---
+++ 0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{92:function(n,o,p){"use strict";var t=p(2).c.button.withConfig({displayName:"Button",componentId:"g4pyz8-0"})(["width:48px;height:32px;border-radius:4px;"]);o.a=t}}]);
+//# sourceMappingURL=0.js.map
--- src/components/Button/index.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Button = styled.button`
-  width: 48px;
-  height: 32px;
-  border-radius: 4px;
-`;
-
-export default Button;
