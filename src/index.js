811806b7708a832a6ab5529818f361b127ddac59
Simon D <simon.douchy@gmail.com>
2020-01-01 13:46:36 +0100

    webpack optimisation / hash / workbox (#19)

--- src/index.js
+++ src/index.js
@@ -49,3 +49,16 @@ const App = () => {
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(registration => {
+        console.log('SW registered: ', registration);
+      })
+      .catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
