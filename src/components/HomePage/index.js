dd50e2721f6147624113647090b1f06f79e29390
Simon Douchy <simon.douchy@gmail.com>
2020-01-05 09:30:52 +0100

    add react-rsponsive

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import HeaderBar from 'components/HeaderBar';
+
+const PageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  color: black;
+  margin: 12px;
+`;
+
+const HomePage = () => {
+  return (
+    <PageContainer>
+      <HeaderBar title="title" />
+      Testttttt
+    </PageContainer>
+  );
+};
+
+export default HomePage;
